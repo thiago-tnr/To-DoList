f2376b7a1fa38399e659f38b7b2afe60aa908b19
Thiago Rocha <77621551+thiago-tnr@users.noreply.github.com>
2021-09-21 00:22:49 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+const [tasks, setTasks] = useState([]);
+const [modal, setModal] = useState(false);
+const [newTask, setNewTask] = useState('');
+
+ const handleTask = (e) => {
+    setNewTask(e.target.value);
+ }
+
+const savetasks = () => {
+  setTasks([...tasks,
+  { id: Math.floor(Math.random()*100),
+    task:newTask,
+    done:false
+    }
+  ]);
+  clear();
+  setModal(false);
+};
+
+const clear = () =>{
+  setNewTask('')
+}
+
+const openModal = () =>{
+  setModal(!modal)
+}
+
+
+const toDone = (id) => {
+  let riskTask = tasks.filter(function(risk){
+      if(risk.id == id){
+        risk.done = true;
+        return risk;
+      }
+  })
+  setTasks(riskTask)
+ };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        modal &&
+        <div className='modal'>
+            <div className='open--modal'>
+              <h3>O que vamos fazer ?</h3>
+              <input value={newTask} onChange={handleTask} className='add--task' type='text'/>
+              { newTask.length > 0 &&
+                 <button onClick={savetasks}>Bora fazer</button>
+              }
+            </div>
+        </div>
+      }
+        <div onClick={openModal} className='tasks-todo'>
+          +
+        </div>
+        <div className='tasks'>
+          <h2>Coisas para fazer</h2>
+          {tasks.map((toDo, index)=>{
+            if(toDo.done){
+                return(
+                  <p key={index} onClick={()=>toDone(toDo.id)} style={{textDecoration:'line-through'}}>{toDo.task}</p>
+              );
+             }else{
+                return(
+                  <p onClick={()=>toDone(toDo.id)}>{toDo.task}</p>
+                );
+          }
+        })}
+        </div>
     </div>
   );
 }
